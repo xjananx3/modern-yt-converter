4c79fe5edc760b4d86e20122dded1fb3fe47ae87
jack <janan9980@gmail.com>
2024-08-22 16:02:58 +0200

    Added UI

---
+++ YouTubeDownloader/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace YouTubeDownloader;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DownloadButton_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
