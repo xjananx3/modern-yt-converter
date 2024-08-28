fc9da3d693865942a1f170b4cca01c886d31504b
jack <janan9980@gmail.com>
2024-08-28 14:36:56 +0200

    refactor: use string.Format for quality string

--- YouTubeDownloader/MainPage.xaml.cs
+++ YouTubeDownloader/MainPage.xaml.cs
@@ -85,30 +85,28 @@ public sealed partial class MainPage : Page
     private string GetQualityArgument()
     {
         string quality = ((ComboBoxItem)QualityComboBox.SelectedItem).Content.ToString()!;
-        string extension = GetVideoExtension();
+        string videoExtension = GetVideoExtension();
         string videoAudioExtension = GetVideoAudioExtension();
         
         return quality switch
         {
-            "480p" => $"bestvideo{extension}[height<=480]+bestaudio{videoAudioExtension}/best{extension}[height<=480]",
-            "720p" => $"bestvideo{extension}[height<=720]+bestaudio{videoAudioExtension}/best{extension}[height<=720]",
-            "1080p" => $"bestvideo{extension}[height<=1080]+bestaudio{videoAudioExtension}/best{extension}[height<=1080]",
-            "Best" => $"bestvideo{extension}+bestaudio{videoAudioExtension}/best{extension}/best",
-            _ => ""
+            "480p" => string.Format(Format480p, videoExtension, videoAudioExtension, videoExtension),
+            "720p" => string.Format(Format720p, videoExtension, videoAudioExtension, videoExtension),
+            "1080p" => string.Format(Format1080p, videoExtension, videoAudioExtension, videoExtension),
+            "Best" => string.Format(FormatBest, videoExtension, videoAudioExtension, videoExtension),
+            _ => string.Empty
         };
     }
 
     private string GetVideoExtension()
     {
         bool isWebMFormat = (bool)WebMFormatCheckBox.IsChecked;
-        
         return isWebMFormat ? "" : "[ext=mp4]";
     }
 
     private string GetVideoAudioExtension()
     {
         bool isWebMFormat = (bool)WebMFormatCheckBox.IsChecked;
-        
         return isWebMFormat ? "" : "[ext=m4a]";
     }
 
@@ -116,4 +114,9 @@ public sealed partial class MainPage : Page
     {
         _cancellationTokenSource?.Cancel();
     }
+    
+    private const string Format480p = "bestvideo{0}[height<=480]+bestaudio{1}/best{2}[height<=480]";
+    private const string Format720p = "bestvideo{0}[height<=720]+bestaudio{1}/best{2}[height<=720]";
+    private const string Format1080p = "bestvideo{0}[height<=1080]+bestaudio{1}/best{2}[height<=1080]";
+    private const string FormatBest = "bestvideo{0}+bestaudio{1}/best{2}/best";
 }
